2513c43f3c986c0419e771305780f12bee5f2a70
yawata888 <hidekiyawata888@gmail.com>
2024-06-08 11:02:00 -0400

    nvp of solar system basic

---
+++ src/test/java/SolarSystem/SolarSystem.java
@@ -0,0 +1,11 @@
+package SolarSystem;
+
+public class SolarSystem {
+    public static void main(String[] args) {
+        solarSystemBase solar=new solarSystemBase();
+        solar.sun="1";
+        solar.planet="2";
+        solar.star="2";
+        System.out.println("The solar system is ready ");
+    }
+}
